561090ec9b6745526a4c1ece4e64fcfc297a95fa
jhubbell89 <ewhs4jamesh@hotmail.com>
2023-02-28 13:19:00 -0800

    work on destinations page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,34 +1,62 @@
-
-
-
-
 export default function Home() {
   return (
     <div className="bg-red-100">
       <h1>Home</h1>
-      <section>
+      <section className="h-80 bg-[url('./svg-files/layered-peaks-haikei.svg')] ">
         <h1>New Sections</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores, consequatur ullam. Amet voluptates velit reprehenderit aperiam tenetur sit doloribus a nam autem dicta earum consectetur, esse laboriosam, temporibus error debitis.</p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores,
+          consequatur ullam. Amet voluptates velit reprehenderit aperiam tenetur
+          sit doloribus a nam autem dicta earum consectetur, esse laboriosam,
+          temporibus error debitis.
+        </p>
+        
       </section>
-      <section>
+<div className=""> </div>
+      <section className="h-80">
         <h1>New Sections</h1>
-        <p>Voluptatem officiis dolor iste in mollitia dolorem debitis eligendi labore qui assumenda. Maxime consequuntur nobis perspiciatis hic, exercitationem provident sed praesentium nisi minus sequi fugiat quasi possimus assumenda tempora molestiae.</p>
+        <p>
+          Voluptatem officiis dolor iste in mollitia dolorem debitis eligendi
+          labore qui assumenda. Maxime consequuntur nobis perspiciatis hic,
+          exercitationem provident sed praesentium nisi minus sequi fugiat quasi
+          possimus assumenda tempora molestiae.
+        </p>
       </section>
-      <section>
+      <section className="h-80">
         <h1>New Sections</h1>
-        <p>Excepturi consequatur similique itaque inventore est ipsam, recusandae odit ullam ipsum, nemo quas repellat? Adipisci magnam commodi inventore soluta repellendus doloremque ipsum natus deserunt voluptatem, ipsa eaque, laudantium quis possimus.</p>
+        <p>
+          Excepturi consequatur similique itaque inventore est ipsam, recusandae
+          odit ullam ipsum, nemo quas repellat? Adipisci magnam commodi
+          inventore soluta repellendus doloremque ipsum natus deserunt
+          voluptatem, ipsa eaque, laudantium quis possimus.
+        </p>
       </section>
-      <section>
+      <section className="h-80">
         <h1>New Sections</h1>
-        <p>Similique ipsa exercitationem nesciunt saepe! Corporis dolorum ex reprehenderit incidunt numquam animi voluptatum, a, quam eius commodi modi qui tempora magnam blanditiis distinctio consequuntur ullam earum nesciunt sit voluptas cum.</p>
+        <p>
+          Similique ipsa exercitationem nesciunt saepe! Corporis dolorum ex
+          reprehenderit incidunt numquam animi voluptatum, a, quam eius commodi
+          modi qui tempora magnam blanditiis distinctio consequuntur ullam earum
+          nesciunt sit voluptas cum.
+        </p>
       </section>
-      <section>
+      <section className="h-80">
         <h1>New Sections</h1>
-        <p>Corporis blanditiis id quibusdam quia, rem omnis iusto sapiente, tenetur consectetur quasi ea aut deserunt repudiandae ad hic quidem esse assumenda error ullam, fugiat tempora atque nobis ipsa! Tempore, illum?</p>
+        <p>
+          Corporis blanditiis id quibusdam quia, rem omnis iusto sapiente,
+          tenetur consectetur quasi ea aut deserunt repudiandae ad hic quidem
+          esse assumenda error ullam, fugiat tempora atque nobis ipsa! Tempore,
+          illum?
+        </p>
       </section>
-      <section>
+      <section className="h-80">
         <h1>New Sections</h1>
-        <p>Unde mollitia natus quidem ratione distinctio necessitatibus totam, ut cum veritatis ex expedita, ad beatae eligendi id perspiciatis voluptates dolor corporis aliquid incidunt alias sunt modi? Rem corrupti fugiat officiis?</p>
+        <p>
+          Unde mollitia natus quidem ratione distinctio necessitatibus totam, ut
+          cum veritatis ex expedita, ad beatae eligendi id perspiciatis
+          voluptates dolor corporis aliquid incidunt alias sunt modi? Rem
+          corrupti fugiat officiis?
+        </p>
       </section>
     </div>
   );
